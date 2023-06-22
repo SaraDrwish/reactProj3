3938941f62678dd5e059e0e49a48f33e3ec44108
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-06-22 12:59:58 +0300

    4

--- src/Componants/Works/Works.jsx
+++ src/Componants/Works/Works.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import "./works.css"
+import img1 from "../../img/rolx.png"
+import img2 from "../../img/subway.png"
+import img3 from "../../img/payappl.png"
+import img4 from "../../img/play.png"
+import img5 from "../../img/suzuki.png"
+
+
 
 function Works() {
   return (
@@ -20,7 +27,29 @@ function Works() {
 
           <div className="right-works">
               
+              <div className="works-mainCircl">
+                  
+                  <div className="works-secndCircl">
+                      <img src={img1} alt="" />
+                  </div>
+                  <div className="works-secndCircl">
+                      <img src={img2} alt="" />
+                  </div>
+                  <div className="works-secndCircl">
+                      <img src={img3} alt="" />
+                  </div>
+                  <div className="works-secndCircl">
+                      <img src={img4} alt="" />
+                  </div>
+                  <div className="works-secndCircl">
+                      <img src={img5} alt="" />
+                  </div>
+                  
+              </div>
+              <div className="works-backCicl bluCircl"></div>
+              <div className="works-backCicl orngCircl"></div>
               
+
           </div>
 
 
