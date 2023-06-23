431a2e6a4dbc5b562aa28c485a188d271a45fcaa
Sara <38770404+SaraDrwish@users.noreply.github.com>
2023-06-23 15:18:58 +0300

    footr

--- src/Componants/Footer/Footer.jsx
+++ src/Componants/Footer/Footer.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import "./footer.css"
-
+import Insta from "@iconscout/react-unicons/icons/uil-instagram"
+import Git from "@iconscout/react-unicons/icons/uil-github"
+import Facebook from "@iconscout/react-unicons/icons/uil-facebook"
 
 function Footer() {
   return (
     <div className='footer'>
 
-      <div className="ficons">
+      <div className="fcont">
+        {/* <span>it.saradarwish@gmail.com</span> */}
+        <span> Sara Darwish</span>
+        <div className="ficn">
+          <Insta size="3rem"   />
+          <Git size="3rem"  />
+          <Facebook size="3rem"  />
+        </div>
 
 
       </div>
@@ -20,7 +29,7 @@ function Footer() {
       </div>
 
 
-      
+
       
     </div>
   )
